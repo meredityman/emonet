3fb5ca6cb70e8237c77df36ca91aa05ed8aa6b55
Meredith Thomas <meredityman@gmail.com>
2023-02-27 00:05:01 +0100

    WC

--- ana_main.py
+++ ana_main.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--nclasses', type=int, default=5, choices=[5,8], help='Number of emotional classes to test the model on. Please use 5 or 8.')
 parser.add_argument('--cascPath', default="haarcascade_frontalface_default.xml")
 parser.add_argument('--device'  , type=int, default=0)
-parser.add_argument('--subIp'   , default="127.0.0.1")
+parser.add_argument('--subIp'   , default="0.0.0.0")
 parser.add_argument('--subPort' , default=8104)
 
 args = parser.parse_args()
@@ -33,17 +33,72 @@ if args.nclasses == 8:
 elif args.nclasses == 5: 
     _expressions = {0: 'neutral', 1:'happy', 2:'sad', 3:'surprise', 4:'fear'}
 
+
+class FaceExtractor:
+
+    def __init__(self, cascPath):
+        print("Initialising face extractor")
+        self.faceCascade   = cv2.CascadeClassifier(cascPath)
+        self.face = None
+        self.box = (0.0,0.0,0.0,0.0)
+
+    def ready(self):
+        pass
+
+    def get_face(self, frame):
+        self.frame = frame
+        newFace = self._get_face()
+        return self.face, self.box, newFace
+    
+    def _get_face(self):
+        frame = self.frame
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.5,
+            minNeighbors=5,
+            minSize=(100, 100),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        if len(faces) >= 1:
+            if len(faces) > 1: 
+                print(f"Warning: Multpile faces")
+
+            center_offset = []
+            for (x, y, w, h) in faces:
+                # cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                centroid = np.array([x + w * 0.5, y + h * 0.5])
+                offset = (np.array(frame.shape[:2]) * 0.5) - centroid
+                offset = np.linalg.norm(offset)
+                center_offset.append( offset )
+
+            # print(center_offset)
+            frame_i = np.argmin(np.array(center_offset))
+
+            (x, y, w, h) = faces[frame_i]
+            face =  frame[y:y+h, x:x+w]
+
+            self.face = face
+            self.box  = (x, y, w, h)
+            return True
+        else:
+            return False
+
+
 class EmotionDetector():
 
     def __init__(self, device, n_expression):
+        print("Initializing emotion detector")
         self.device = device
 
         state_dict_path = Path(__file__).parent.joinpath('pretrained', f'emonet_{n_expression}.pth')
         state_dict = torch.load(str(state_dict_path), map_location='cpu')
         state_dict = {k.replace('module.',''):v for k,v in state_dict.items()}
-        self.net = EmoNet(n_expression=n_expression).to(device)
+        print("Loading network...")
+        self.net = EmoNet(n_expression=n_expression).half().to(device)
+        print("Loading state dict...")
         self.net.load_state_dict(state_dict, strict=False)
-        self.net = self.net.half()
         self.net.eval()
 
         self.face  = None
@@ -94,83 +149,28 @@ class EmotionDetector():
         self.expr = expr
         self.expressions = expressions
 
-class FaceExtractor:
-
-    def __init__(self, cascPath):
-        self.faceCascade   = cv2.CascadeClassifier(cascPath)
-        self.face = None
-        self.box = (0.0,0.0,0.0,0.0)
-
-    def ready(self):
-        pass
-
-    def get_face(self, frame):
-        self.frame = frame
-
-        self._get_face()
-
-        return self.face, self.box
-    
-    def _get_face(self):
-        frame = self.frame
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = self.faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.5,
-            minNeighbors=5,
-            minSize=(100, 100),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-
-        if len(faces) >= 1:
-            if len(faces) > 1: 
-                print(f"Warning: Multpile faces")
-
-            center_offset = []
-            for (x, y, w, h) in faces:
-                # cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                centroid = np.array([x + w * 0.5, y + h * 0.5])
-                offset = (np.array(frame.shape[:2]) * 0.5) - centroid
-                offset = np.linalg.norm(offset)
-                center_offset.append( offset )
-
-            # print(center_offset)
-            frame_i = np.argmin(np.array(center_offset))
-
-            (x, y, w, h) = faces[frame_i]
-            face =  frame[y:y+h, x:x+w]
-
-            self.face = face
-            self.box  = (x, y, w, h)
-
-
-
 def main():
     inference_device = 'cuda:0'
     image_size = 256
     
-
     cascPath      = str(Path(args.cascPath).absolute())
     video_capture = cv2.VideoCapture(args.device, cv2.CAP_V4L2)
-
 
     faceExtractor = FaceExtractor(cascPath)
 
     emotionDetector = EmotionDetector(device=inference_device, n_expression=args.nclasses)
 
-
     t0 = time.time()
     while True:
         try:
-
             t0 = time.time()
             ret, frame = video_capture.read()
 
             if frame is not None:
-                face, (x, y, w, h) = faceExtractor.get_face(frame)
+                face, (x, y, w, h), newFace = faceExtractor.get_face(frame)
                 t1 = time.time()
 
-                if face is not None:
+                if face is not None and newFace:
                     # face  = cv2.cvtColor(face , cv2.COLOR_BGR2RGB)
                     face  = cv2.resize(face, (image_size, image_size))
 
@@ -181,7 +181,7 @@ def main():
                         "valance"    : float(val) if val  else 0.0,
                         "arousal"    : float(ar)  if ar   else 0.0,
                         "expression" : expr       if expr else None,
-                        "expression-probability" : expressions[expr] if expr else None,
+                        "expression-probability" : expressions[expr] if 100.0 * expr else None,
                         "logits"     : expressions,
                         "box" : {
                             "x" : int(x),   
@@ -199,13 +199,13 @@ def main():
                     socket.send_string(json.dumps(json_data), 0)
                     t3 = time.time()
 
+                    print(json_data)
                     print(f"fps: {1/(t3-t0):02f} cv: {t1-t0:02f} | emonet: {t2-t1:02f} | zmq: {t3-t2:02f}  | exp: {expr}")
 
 
                     cv2.imshow('face', face)
                     if cv2.waitKey(25) & 0xFF == ord('q'):
                         break
-
             else:
                 print(f"No camera frame!")
                 time.sleep(1.)
